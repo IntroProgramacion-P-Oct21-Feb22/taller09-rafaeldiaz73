eaabb8dcf1eb0827f906143347e4ddee133bd7bd
rafael <erdiaz3@utpl.edu.ec>
2021-12-17 12:31:59 -0500

    estos son los cambios

--- EjerciciosT9/src/paquete1/Ejercicio00.java
+++ EjerciciosT9/src/paquete1/Ejercicio00.java
@@ -12,4 +12,43 @@ package paquete1;
  */
 public class Ejercicio00 {
     
+
+public static void main(String[] args) {
+        // Declarar y generar arreglos en Java
+
+        // Se declara un arreglo de tipo double, llamado promedios
+        double[] promediosMateria1 = {8.1, 9, 9.5, 10, 9.9};
+        double[] promediosMateria2 = {7.9, 9.2, 9.7, 9.8, 9.7};
+        String[] estudiantes = {"Luis", "Ana", "Mónica", "José", "Juan"};
+        double[] promediosFinales = new double[5];
+
+        double sumaNotas; //promediossMateria1[2] + promediosMateria2[2] ;
+        double promedio;
+
+        String mensajeFinal = ""; // variable que acumula un reporte final
+        // Se hace uso del ciclo repetitivo for para recorrer los arreglos
+        // y presentar los valore de las posiciones
+
+        for (int i = 0; i < promediosFinales.length; i++) {
+            // se suma el valor de la posición i del arreglo 
+            // promediosMateria1 y promediosMateria2
+            sumaNotas = promediosMateria1[i] + promediosMateria2[i];
+            // se obtiene el promedio de la suma de las notas
+            promedio = sumaNotas / 2;
+            // se asigna a la posición i del arreglo promediosFinales
+            promediosFinales[i] = promedio;
+        }
+
+        for (int i = 0; i < promediosMateria1.length; i++) {
+
+            mensajeFinal = String.format("%sEstudiante:%s - promedio final: "
+                    + "%.4f\n",
+                    mensajeFinal,
+                    estudiantes[i],
+                    promediosFinales[i]);
+        }
+
+        System.out.printf("%s\n", mensajeFinal);
+
+    }
 }
--- EjerciciosT9/src/paquete1/Ejercicio03.java
+++ EjerciciosT9/src/paquete1/Ejercicio03.java
@@ -17,9 +17,28 @@ public class Ejercicio03 {
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        int elementosArribaMedia;
-        int elementosAbajoMedia;
+        int elementosArribaMedia = 0;
+        int elementosAbajoMedia = 0;
+        double media = 0;
+        double suma = 0;
         
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+        }
+        
+        media = suma / arreglo.length;
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] < media) {
+                elementosAbajoMedia = elementosAbajoMedia + 1;}
+                else {   
+            }
+            if (arreglo[i] > media) {
+                elementosArribaMedia = elementosArribaMedia + 1;
+            }
+        }
+        System.out.printf("media:%.2f\n elementos Arriba Media:%d"
+                        +" \n elementos Abajo Media:%d"
+                        , media, elementosArribaMedia, elementosAbajoMedia);
     }
 
 }
